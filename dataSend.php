297e5d16f42795825144ab3bb4896e964adf779b
dlddu <csj6922@naver.com>
2018-05-24 19:36:49 +0900

    마커 종류별로 저장되게 변경

--- dataSend.php
+++ dataSend.php
@@ -5,7 +5,9 @@
 	
 	require 'openMysql.php';
 	
-	$query = $sql->prepare('INSERT INTO '.$table' ('.$col.') VALUE (?)');
+	$sql = open();
+	
+	$query = $sql->prepare('INSERT INTO '.$table.' ('.$col.') VALUE (?)');
 	$query->execute(array($data));
 	
 	$sql = null;
