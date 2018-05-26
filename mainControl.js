8d65c4a567dec527af9a73e87c5a4d3e16a80913
dlddu <csj6922@naver.com>
2018-05-26 23:26:55 +0900

    지승님 브랜치 merge 시도1

--- mainControl.js
+++ mainControl.js
@@ -19,6 +19,15 @@ document.getElementById("setMarker").addEventListener("click", function() {
 
 document.getElementById("setGeolocation").addEventListener("click", geoLocation);
 
+/*
+document.getElementById("setArea").addEventListener("click", function() {
+	changeMarker("area");
+	document.getElementById("setArea").style.display = 'none';
+	document.getElementById("createPolyline").style.display = 'inline';
+	});
+document.getElementById("createPolyline").addEventListener("click", addPolygon);
+*/
+	
 // 지도에 클릭 이벤트를 등록합니다
 daum.maps.event.addListener(map, 'click', function(mouseEvent) {        
 	// 클릭한 위도, 경도 정보를 가져옵니다 
@@ -26,5 +35,5 @@ daum.maps.event.addListener(map, 'click', function(mouseEvent) {
 	// 마커 위치를 클릭한 위치로 옮깁니다
 	iwContent = '<div style="padding:5px;">Hello World!</div>'; // 인포윈도우에 표출될 내용으로 HTML 문자열이나 document element가 가능합니다	
 	addMarker(latlng, iwContent, type);
-	saveMakrer(latlng, iwContent, type);
+	saveMarker(latlng, iwContent, type);
 });
