43e773efe29ce0dacf706fc31daa91f39400b8fc
234wen <39252016+234wen@users.noreply.github.com>
2018-05-28 17:07:36 +0900

    Update daumApi.js

--- daumApi.js
+++ daumApi.js
@@ -136,7 +136,7 @@ function addMarker(position, iwContent) {
 
 		
     var imageSrc1 = 'images/Icon_Skateboard(150x150).png', // 마커이미지의 주소입니다    
-    imageSize1 = new daum.maps.Size(64, 69), // 마커이미지의 크기입니다
+    imageSize1 = new daum.maps.Size(22, 24), // 마커이미지의 크기입니다
     imageOption1 = {offset: new daum.maps.Point(27, 69)}; // 마커이미지의 옵션입니다. 마커의 좌표와 일치시킬 이미지 안에서의 좌표를 설정합니다.
       
     // 마커의 이미지정보를 가지고 있는 마커이미지를 생성합니다
@@ -158,7 +158,7 @@ function addMarker(position, iwContent) {
 		
 		
     var imageSrc2 = 'images/Icon_Basketball(150x150).png', // 마커이미지의 주소입니다    
-    imageSize2 = new daum.maps.Size(64, 69), // 마커이미지의 크기입니다
+    imageSize2 = new daum.maps.Size(22, 24), // 마커이미지의 크기입니다
     imageOption2 = {offset: new daum.maps.Point(27, 69)}; // 마커이미지의 옵션입니다. 마커의 좌표와 일치시킬 이미지 안에서의 좌표를 설정합니다.
       
     // 마커의 이미지정보를 가지고 있는 마커이미지를 생성합니다
