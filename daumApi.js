50a3c6978da892b37f76f6552119593196293208
234wen <39252016+234wen@users.noreply.github.com>
2018-05-29 14:12:07 +0900

    Update daumApi.js

--- daumApi.js
+++ daumApi.js
@@ -136,7 +136,7 @@ function addMarker(position, iwContent) {
               var imageSrc1 = 'images/Icon_Skateboard(150x150).png', // 마커이미지의 주소입니다    
               imageSize1 = new daum.maps.Size(22, 24), // 마커이미지의 크기입니다
              // imageOption1 = {offset: new daum.maps.Point(27, 69)}; // 마커이미지의 옵션입니다. 마커의 좌표와 일치시킬 이미지 안에서의 좌표를 설정합니다.
-        imageOption1 = { 
+              imageOption1 = { 
 		center: new daum.maps.LatLng(33.450701, 126.570667), //지도의 중심좌표.
 		level: 3 //지도의 레벨(확대, 축소 정도)
 	};
@@ -160,8 +160,10 @@ function addMarker(position, iwContent) {
 		
                var imageSrc2 = 'images/Icon_Basketball(150x150).png', // 마커이미지의 주소입니다    
                imageSize2 = new daum.maps.Size(22, 24), // 마커이미지의 크기입니다
-               imageOption2 = {offset: new daum.maps.Point(27, 69)}; // 마커이미지의 옵션입니다. 마커의 좌표와 일치시킬 이미지 안에서의 좌표를 설정합니다.
-      
+               imageOption2 = { // 마커이미지의 옵션입니다. 마커의 좌표와 일치시킬 이미지 안에서의 좌표를 설정합니다.
+		center: new daum.maps.LatLng(33.450701, 126.570667), //지도의 중심좌표.
+		level: 3 //지도의 레벨(확대, 축소 정도)
+	};
     // 마커의 이미지정보를 가지고 있는 마커이미지를 생성합니다
                var markerImage2 = new daum.maps.MarkerImage(imageSrc2, imageSize2, imageOption2),
                markerPosition2 = marker.getPosition();// 마커가 표시될 위치입니다
@@ -181,7 +183,10 @@ function addMarker(position, iwContent) {
 		
     var imageSrc3 = 'images/Icon_Fishing(150x150).png', // 마커이미지의 주소입니다    
     imageSize3 = new daum.maps.Size(22, 24), // 마커이미지의 크기입니다
-    imageOption3 = {offset: new daum.maps.Point(27, 69)}; // 마커이미지의 옵션입니다. 마커의 좌표와 일치시킬 이미지 안에서의 좌표를 설정합니다.
+    imageOption3 = {        // 마커이미지의 옵션입니다. 마커의 좌표와 일치시킬 이미지 안에서의 좌표를 설정합니다.
+		center: new daum.maps.LatLng(33.450701, 126.570667), //지도의 중심좌표.
+		level: 3 //지도의 레벨(확대, 축소 정도)
+	}; // 마커이미지의 옵션입니다. 마커의 좌표와 일치시킬 이미지 안에서의 좌표를 설정합니다.
       
     // 마커의 이미지정보를 가지고 있는 마커이미지를 생성합니다
     var markerImage3 = new daum.maps.MarkerImage(imageSrc3, imageSize3, imageOption3),
