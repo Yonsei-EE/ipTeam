13b03b80b01ae0269502cf009014f3801e854343
iseikr <patrikseoul@gmail.com>
2018-06-11 06:53:35 +0900

    push

--- index.php
+++ index.php
@@ -114,7 +114,7 @@ if(isset($_GET['code'])) {
 								<button id="insta_button" class="btn btn-default btn-block" style="font-size:0.9em;" type="button">Instagram</button>
 							</div>
 						</div>
-						<div class="row border rounded" >
+						<div class="row" >
 							<?php
 								if ($_SESSION['logged_in'] == 0){
 									echo "<a href={$login_url}> <img src=\"images/insta_signin.png\" /> </a>";
@@ -123,7 +123,7 @@ if(isset($_GET['code'])) {
 
 									echo "<div class=\"col\"><img style=\"width=\"64\" class='img-thumbnail' src=\"{$user_info["profile_picture"]}\" /> </div>"; 
 									echo "<div class =\"row\">";
-									echo "<div class=\"col-sm-2 small\">{$user_info["username"]} </div>"; 
+									echo "<div class=\"col-sm small\">{$user_info["username"]} </div>"; 
 									echo "</div>";
 
 								
